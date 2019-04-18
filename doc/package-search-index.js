dea6a802c200cb275d07f9574230d76807d659a7
Bach Vu <bach.vuviet01@gmail.com>
2019-04-18 22:44:33 +1200

    Complete Javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Backend"},{"l":"CustomUIELmt"},{"l":"GUI"},{"l":"SpaceVessel"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Backend"},{"l":"CustomUIELmt"},{"l":"GUI"},{"l":"SpaceVessel"},{"l":"UnitTest"}]
---
+++ src/SpaceVessel/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * This package contain classes about Outpost, Spaceship, and Crew. The player have decision on these objection. 
+ * @author Bach Vu, Linh Luu
+ * @since 0.20
+ * @version 0.30
+ */
+package SpaceVessel;
