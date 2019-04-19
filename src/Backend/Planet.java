2866ca32df5df2e34d618ab3d440662374c66e1a
Bach Vu <bach.vuviet01@gmail.com>
2019-04-19 19:50:35 +1200

    test conflict

--- src/Backend/Planet.java
+++ src/Backend/Planet.java
@@ -31,28 +31,29 @@ public class Planet extends Entity implements CelestialBody {
 	public void setHiddenTreasure() {
 		for (int i=1; i <= 20; i++) {
 			int val = num.nextInt(i);
-		Stock food1 = new Stock_Food("Burger", val, "/burger.png");
-		Stock food2 = new Stock_Food("Bread", val, "/bread.png");
-		Stock food3 = new Stock_Food("Pizza", val, "/pizza.png");
-		Stock food4 = new Stock_Food("Chicken", val, "/Chicken.png");
-		Stock food5 = new Stock_Food("Steak", val, "/steak.png");
-		Stock food6 = new Stock_Food("Sushi", val, "/sushi.png");
-		
-		Stock medi1 = new Stock_Medicine("Healing Potion", "Heart", val, "/healpotion.png");
-		Stock medi2 = new Stock_Medicine("Pain Killer", "Morale", val, "/painkiller.png");
-		Stock medi3 = new Stock_Medicine("Syringe", "Any disease", val, "/syringe.png");
-		
-		ArrayList<Stock> STOCK = new ArrayList<Stock>();
-		STOCK.add(food1);STOCK.add(food2);
-		STOCK.add(food3);STOCK.add(food4);
-		STOCK.add(food5);STOCK.add(food6);
-		STOCK.add(medi1);STOCK.add(medi2);
-		STOCK.add(medi3);
-		
-		Collections.shuffle(STOCK);
-		for (Stock st: STOCK) {
-			hiddenStock = st;
-		}}
+			Stock food1 = new Stock_Food("Burger", val, "/burger.png");
+			Stock food2 = new Stock_Food("Bread", val, "/bread.png");
+			Stock food3 = new Stock_Food("Pizza", val, "/pizza.png");
+			Stock food4 = new Stock_Food("Chicken", val, "/Chicken.png");
+			Stock food5 = new Stock_Food("Steak", val, "/steak.png");
+			Stock food6 = new Stock_Food("Sushi", val, "/sushi.png");
+			
+			Stock medi1 = new Stock_Medicine("Healing Potion", "Heart", val, "/healpotion.png");
+			Stock medi2 = new Stock_Medicine("Pain Killer", "Morale", val, "/painkiller.png");
+			Stock medi3 = new Stock_Medicine("Syringe", "Any disease", val, "/syringe.png");
+			
+			ArrayList<Stock> STOCK = new ArrayList<Stock>();
+			STOCK.add(food1);STOCK.add(food2);
+			STOCK.add(food3);STOCK.add(food4);
+			STOCK.add(food5);STOCK.add(food6);
+			STOCK.add(medi1);STOCK.add(medi2);
+			STOCK.add(medi3);
+			
+			Collections.shuffle(STOCK);
+			for (Stock st: STOCK) {
+				hiddenStock = st;
+			}
+		}
 		//System.out.println(STOCK);
 		//System.out.println(val);
 	}
